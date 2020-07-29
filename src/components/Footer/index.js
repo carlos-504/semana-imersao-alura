6b821ca71a36d8be8f65581db98d2173d5fd07b7
Carlos Henrique <carllosshennriique@gmail.com>
2020-07-28 23:30:11 -0300

    Segundo dia - Commit 3

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Logo from '../../assets/img/Logo.png';
 import { FooterBase } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+        <img className="Logo" src={Logo} alt="CarlaoFlix logo" />
       <p>
         Orgulhosamente criado durante a
         {' '}
